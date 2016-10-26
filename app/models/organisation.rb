6b694b675aab7c510b85a16b564956889711be44
Laurie Skelly <laurieskelly@users.noreply.github.com>
2016-10-26 17:01:11 -0500

    adding website format validation (without requiring website presence) on organisation model

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -2,4 +2,7 @@ class Organisation < ApplicationRecord
   has_many :users
   has_many :locations
   has_many :documents, through: :users
+
+  validates :website, format: { with: URI.regexp }, if: 'website.present?'
+
 end
